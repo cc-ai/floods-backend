87ed4a8a7ac98cd89e7ae6e7425af6641b13981f
François Corneau-Tremblay <corneau90@gmail.com>
2019-05-14 13:50:12 -0400

    Remove test for group_code

--- ccai/tests/test_engine.py
+++ ccai/tests/test_engine.py
@@ -2,20 +2,31 @@
 import copy
 from unittest.mock import patch
 
-from ccai.app.engine import find_location
+from ccai.app.engine import find_location, get_unique_id
 from googlegeocoder import GeocoderResult
 
 
-def test_find_location_without_group_code(mock_location_no_gc, mock_location_hash):
-    """Test the `find_location` function where the geocoder result has no group_code."""
+def test_get_unique_id(mock_location, mock_location_hash):
+    """Test `get_unique_id` function."""
+    location = ['geometry']['location']
+    lat = location['lat']
+    lng = location['lng']
+    location_dict = {'latitude': lat, 'longitude': lng}
+    _id = get_unique_id(location_dict)
+
+    assert _id == mock_location_hash
+
+
+def test_find_location(mock_location, mock_location_hash):
+    """Test the `find_location` function."""
     with patch("ccai.app.engine.GoogleGeocoder", key="") as mock:
-        geocoder_result = GeocoderResult(copy.deepcopy(mock_location_no_gc))
+        geocoder_result = GeocoderResult(copy.deepcopy(mock_location))
         geocoder = mock.return_value
         geocoder.get.return_value = [geocoder_result]
 
         full_location = find_location("mila")
 
         assert full_location['address'] == "mila"
-        assert full_location['latitude'] == mock_location_no_gc['geometry']['location']['lat']
-        assert full_location['longitude'] == mock_location_no_gc['geometry']['location']['lng']
+        assert full_location['latitude'] == mock_location['geometry']['location']['lat']
+        assert full_location['longitude'] == mock_location['geometry']['location']['lng']
         assert full_location['_id'] == mock_location_hash
