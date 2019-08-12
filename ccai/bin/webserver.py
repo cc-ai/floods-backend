ebd4eea4b1f45f1db716ce5ebfd235a27d212344
Mike Arpaia <mike@arpaia.co>
2019-08-12 17:19:03 -0400

    Updates for linting and type checking

--- ccai/bin/webserver.py
+++ ccai/bin/webserver.py
@@ -4,6 +4,8 @@
 A web service for indexing and retrieving documents
 """
 
+# pylint: disable=R0914
+
 import logging
 import os
 import tempfile
@@ -13,39 +15,37 @@ from flask_cors import CORS
 from PIL import Image
 import prometheus_client
 import torch
-from torch.autograd import Variable
-from torchvision import transforms
 import torchvision.utils as vutils
+from torchvision import transforms
 
 from ccai.config import CONFIG
 from ccai.nn.munit.trainer import MUNIT_Trainer
 from ccai.streetview import fetch_street_view_image
 
+# Global environment-based configuration
 DEBUG = os.environ.get("DEBUG", False)
+
+# Versions of models that are supported by the API endpoint
 VALID_VERSIONS = ["munit"]
 
+# API server initialization
 app = Flask(__name__)  # pylint: disable=C0103
 CORS(app)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO)
 
-max_iter = CONFIG.munit_config["max_iter"]
-display_size = CONFIG.munit_config["display_size"]
-new_size = CONFIG.munit_config["new_size"]
-opts_trainer = "MUNIT"
-opts_style = ""
-opts_a2b = 1
-opts_n_style = 100
-opts_output_folder = "./output_interpolate/"
 
-style_dim = CONFIG.munit_config["gen"]["style_dim"]
-trainer = MUNIT_Trainer(CONFIG.munit_config)
-state_dict = torch.load(
-    CONFIG.MUNIT_CHECKPOINT_FILE, map_location={"cuda:0": "cpu"}
-)  # TODO: only map when GPU is not available
-trainer.gen.load_state_dict(state_dict["2"])
-# trainer.cuda() # TODO: enable cuda if available
-trainer.eval()
+# MUNIT Hyperparameters and Model
+NEW_SIZE = CONFIG.munit_config["new_size"]
+MUNIT_MODEL = MUNIT_Trainer(CONFIG.munit_config)
+if torch.cuda.is_available():
+    MUNIT_STATE_DICT = torch.load(CONFIG.MUNIT_CHECKPOINT_FILE)
+else:
+    MUNIT_STATE_DICT = torch.load(CONFIG.MUNIT_CHECKPOINT_FILE, map_location={"cuda:0": "cpu"})
+MUNIT_MODEL.gen.load_state_dict(MUNIT_STATE_DICT["2"])
+if torch.cuda.is_available():
+    MUNIT_MODEL.cuda()  # type: ignore
+MUNIT_MODEL.eval()
 
 
 @app.route("/address/<string:version>/<string:address>", methods=["GET"])
@@ -80,7 +80,7 @@ def address2photo(version: str, address: str) -> Response:
         with torch.no_grad():
             transform = transforms.Compose(
                 [
-                    transforms.Resize(new_size),
+                    transforms.Resize(NEW_SIZE),
                     transforms.ToTensor(),
                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                 ]
@@ -89,17 +89,20 @@ def address2photo(version: str, address: str) -> Response:
             # path to streetview image
             path_xa = os.path.join(temp_dir, "gsv_0.jpg")
 
-            x_a = Variable(
-                transform(Image.open(path_xa).convert("RGB")).unsqueeze(0)
-                # .cuda()) # TODO: optionally enable cuda
-            )
-            c_xa_b, s_xa = trainer.gen.encode(x_a, 1)
+            image_transformed = transform(Image.open(path_xa).convert("RGB")).unsqueeze(0)
+            if torch.cuda.is_available():
+                image_transformed = image_transformed.cuda()
+            x_a = torch.Tensor(image_transformed)
+            c_xa_b, _ = MUNIT_MODEL.gen.encode(x_a, 1)
 
             # Initiate parameters
-            c = c_xa_b
-            s = Variable(torch.mul(torch.randn(1, 16, 1, 1), 0.5))  # .cuda()) # TODO: enable cuda
+            content = c_xa_b
+            style_data = torch.mul(torch.randn(1, 16, 1, 1), 0.5)
+            if torch.cuda.is_available():
+                style_data = style_data.cuda()
+            style = torch.Tensor(style_data)
 
-            outputs = trainer.gen.decode(c, s, 2)
+            outputs = MUNIT_MODEL.gen.decode(content, style, 2)
             outputs = (outputs + 1) / 2.0
 
             output_path = os.path.join(temp_dir, "output" + "{:03d}.jpg".format(0))
